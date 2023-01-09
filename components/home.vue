5faf7bafa3ae730866cd92e3fd4e6ba00fdc8f09
layla <122133704+laylastyles@users.noreply.github.com>
2023-01-09 12:12:07 -0500

    trauma day 1

---
+++ components/home.vue
@@ -0,0 +1,9 @@
+<template> 
+    <div> 
+        <h1> Layla Hussein is a storyteller, educator, and activist attending 
+            Columbia University in the City of New York.  </h1>
+        <p> Hello world, I guess </p>
+
+
+    </div>
+</template>
